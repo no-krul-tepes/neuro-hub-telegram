175e15a73ecd6ce3b2fe5ed7d11e489af6d3fab8
ashura <cotopessergo@gmail.com>
2024-12-06 15:20:03 +0300

    first commit

---
+++ api/bot.ts
@@ -0,0 +1,10 @@
+import { Bot, webhookCallback } from "grammy";
+
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is unset");
+
+const bot = new Bot(token);
+
+bot.command("start", (ctx) => ctx.reply("Привет! Я Telegram-бот!"));
+
+export default webhookCallback(bot, "std/http");
