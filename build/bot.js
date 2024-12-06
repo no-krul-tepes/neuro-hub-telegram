87e1fdceefa4f14f03b6b2be86f155a2e7d4b2d3
ashura <cotopessergo@gmail.com>
2024-12-06 15:58:03 +0300

    update gitignore

--- build/bot.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const grammy_1 = require("grammy");
-const token = process.env.BOT_TOKEN;
-if (!token)
-    throw new Error("BOT_TOKEN is unset");
-const bot = new grammy_1.Bot(token);
-bot.command("start", (ctx) => ctx.reply("Привет! Я Telegram-бот!"));
-exports.default = (0, grammy_1.webhookCallback)(bot, "std/http");
